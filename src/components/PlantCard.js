9347a19e3626189be6193db13b042465805646c0
Allison <amh03160@gmail.com>
2022-03-22 09:52:10 -0500

    toggle sold out/in stock

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({plant}) {
+const [inStock, setInStock] = useState(true)
 
+function toggleSoldOut(){
+  setInStock(!inStock)
+}
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={toggleSoldOut}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick ={toggleSoldOut}>Out of Stock</button>
       )}
     </li>
   );
